3b0355ec1b406a7a4ce5d5d5701bd6c29fc1d6cb
IsmaelMastronardi <ismael@makeworldwide.com>
2024-10-12 12:04:31 -0300

    ad getCountryInfo Method

--- back_end/src/country/country.service.ts
+++ back_end/src/country/country.service.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 @Injectable()
 export class CountryService {
   nagerApi = 'https://date.nager.at/api/v3';
+  nowApi = 'https://countriesnow.space/api/v0.1/countries';
 
   async getCountries() {
     try {
@@ -12,6 +13,39 @@ export class CountryService {
     } catch (error) {
       throw new HttpException(
         'Failed to fetch coutries',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async getCountryInfo(countryCode: string) {
+    try {
+      const borderCountriesResponse = await axios.get(
+        `${this.nagerApi}/CountryInfo/${countryCode}`,
+      );
+      const populationResponse = await axios.get(`${this.nowApi}/population`);
+      const flagResponse = await axios.get(`${this.nowApi}/flag/images`);
+
+      const borderCountries = borderCountriesResponse.data?.borders || [];
+
+      const countryName = borderCountriesResponse.data?.commonName;
+
+      const populationData = populationResponse.data.data.find(
+        (country) => country.country === countryName,
+      );
+
+      const flagUrl = flagResponse.data.data.find(
+        (country) => country.iso2 === countryCode,
+      )?.flag;
+
+      return {
+        borderCountries,
+        population: populationData?.populationCounts || 0,
+        flagUrl,
+      };
+    } catch (error) {
+      throw new HttpException(
+        'Failed to fetch country info',
         HttpStatus.BAD_REQUEST,
       );
     }
