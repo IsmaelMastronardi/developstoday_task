47f66a92da89fda8a56b5ddab5c581f5610332e7
IsmaelMastronardi <ismael@makeworldwide.com>
2024-10-12 11:16:45 -0300

    add getCountries method

--- back_end/src/country/country.service.ts
+++ back_end/src/country/country.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class CountryService {}
+export class CountryService {
+  nagerApi = 'https://date.nager.at/api/v3';
+
+  async getCountries() {
+    try {
+      const response = await axios.get(`${this.nagerApi}/AvailableCountries`);
+      return response.data;
+    } catch (error) {
+      throw new HttpException(
+        'Failed to fetch coutries',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}
