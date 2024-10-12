f028f8702d779d894030e374327224288a836824
IsmaelMastronardi <ismael@makeworldwide.com>
2024-10-12 11:15:53 -0300

    add 'all' route

--- back_end/src/country/country.controller.ts
+++ back_end/src/country/country.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CountryService } from './country.service';
 
-@Controller('country')
-export class CountryController {}
+@Controller('countries')
+export class CountryController {
+  constructor(private readonly countryService: CountryService) {}
+
+  @Get('all')
+  async getAllCountries() {
+    return this.countryService.getCountries();
+  }
+}
